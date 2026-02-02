65e0b7887bb6be3dc4a9bec305b919b9871bcc1e
ashutosh mishra <mishraashutosh.dev@gmail.com>
2026-02-02 13:15:55 +0530

    working bank module in simple js fundamentals of redux

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
 /*** 
- yha hamne dekha rahe hai kis prakar redux me state , reducer function aur action pr kaam kiya jata hai
+    yha ham log details me ek bank ka example leke work karenge jaise deposite karna, withdraw, balance check karna
+    
+    reducer method (bankReducer) me state aur action pass kiya ja rha hai action me hame kis type ka action hai use 
+    bhi pass karte hai mean kya use withdraw karna chahta hai ya balance inquery karna chahta hai ya paise ko deposite
+    karna chahta hai
+
+    jab ham paise lene ya dene ki bat kar rhe hai tab ham action ko batana bhi pade ga jise ham payload ke jariye batate hai
+
  */
 
-let initialReduxState = {
-  post: 0,
+let initialState = {
+  balance: 0,
   name: "Ashutosh Mishra",
   age: 28,
 };
 
-let prevState = initialReduxState;
+console.log("current state is : ", initialState);
 
-console.log("prevState : ", prevState);
-
-/**
-    agar ham if ke return object me ...state (spread object ) pass nhi karenge to  initialReduxState me 
-    jo state (post,name,age) hai unme se sirf post state change ho ga baki ka state remove ya delete ho jayega 
-    
-    function reducer(state, action) {
-        if (action.type === "post/increment") {
-        return { post: state.post + 1 };
-        }
-    }
-
- */
-function reducer(state, action) {
-  if (action.type === "post/increment") {
-    return { ...state, post: state.post + 1 };
-  } else if (action.type === "post/decrement") {
-    return { ...state, post: state.post - 1 };
+function bankReducer(state, action) {
+  if (action.type === "bank/deposite") {
+    return { ...state, balance: state.balance + action.payload };
+  } else if (action.type === "bank/withdraw") {
+    return { ...state, balance: state.balance - action.payload };
   }
 
-  //agar ham yha return state nhi return karenge aur user ne mano condition base type pass nhi kiya
-  // to by default undefined return hoga jo ki galat hai esi liye hamesa return state karna chahiye
-  //  jisse hamara last action data pass ho sake
   return state;
 }
 
-initialReduxState = reducer(initialReduxState, { type: "post/increment" });
-console.log("state after increment : ", initialReduxState);
+initialState = bankReducer(initialState, {
+  type: "bank/deposite",
+  payload: 1000,
+});
+console.log("after deposit money : ", initialState);
 
-initialReduxState = reducer(initialReduxState, { type: "post/increment" });
-console.log("state after increment : ", initialReduxState);
+initialState = bankReducer(initialState, {
+  type: "bank/deposite",
+  payload: 1000,
+});
+console.log("after deposit money : ", initialState);
 
-initialReduxState = reducer(initialReduxState, { type: "post/decrement" });
-console.log("state after decrement : ", initialReduxState);
+initialState = bankReducer(initialState, {
+  type: "bank/withdraw",
+  payload: 500,
+});
+console.log("after withdraw money : ", initialState);
 
-initialReduxState = reducer(initialReduxState, { type: "post/abcd" });
-console.log("state after false condition : ", initialReduxState);
+console.log("total balance : ", initialState.balance);
