502a8869bd641851586a8c5579fbfc4408798008
ashutosh mishra <mishraashutosh.dev@gmail.com>
2026-02-03 14:39:48 +0530

    using variable and and naming convension

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
+/** yha hamne action type ke value ko ek variable me store kar diya jise bad me
+ * reducer function aur dispatch me same variable name ke hisab se use kar liya
+ * variable ko CAPITAL letter me hi likhate hai aisa jaruri nhi but likhana chahiye
+ *  */
+
 import { createStore } from "redux";
 
-let initialState = {
+const initialState = {
   balance: 0,
   name: "Ashutosh Mishra",
   age: 28,
 };
 
+const DEPOSITE = "bank/deposite";
+const WITHDRAW = "bank/withdraw";
+
 function reducer(state = initialState, action) {
-  if (action.type === "bank/deposite") {
+  if (action.type === DEPOSITE) {
     return { ...state, balance: state.balance + action.payload };
-  } else if (action.type === "bank/withdraw") {
+  } else if (action.type === WITHDRAW) {
     return { ...state, balance: state.balance - action.payload };
   }
-
   return state;
 }
 
 const store = createStore(reducer);
 console.log("store :", store);
 
-//MUTLIPLE CONSOLE.LOG NA LIKHANA PADE USKE LIYE HAM SUBSCRIBE KA USE KARENGE
-
 store.subscribe(() => {
   console.log(store.getState());
 });
-//console.log("current state :", store.getState());
 
-store.dispatch({ type: "bank/deposite", payload: 2000 });
-//console.log(store.getState());
-
-store.dispatch({ type: "bank/deposite", payload: 3000 });
-//console.log(store.getState());
-
-store.dispatch({ type: "bank/withdraw", payload: 1200 });
-//console.log(store.getState());
+store.dispatch({ type: DEPOSITE, payload: 2000 });
+store.dispatch({ type: DEPOSITE, payload: 3000 });
+store.dispatch({ type: WITHDRAW, payload: 1200 });
